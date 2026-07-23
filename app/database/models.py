4164f4489d45ea1dbbd424d6517ff972b5da37e4
levvis16 <zoomcross2@gmail.com>
2026-07-23 23:04:22 +0300

    add db start architecture

--- app/database/models.py
+++ app/database/models.py
@@ -2,4 +2,60 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, E
 
 from sqlalchemy.orm import relationship
 from database.database import Base
+import enum
+from datetime import datetime
 
+class OrderStatus(str, enum.Enum):
+    PENDING = 'pending'
+    PAID = 'paid'
+    PLAYED = 'played'
+    SKIPPED = 'skipped'
+    CANCELLED = 'cancelled'
+
+class Venue(Base):
+    __tablename__ = 'venues'
+    id = Column(Integer, primary_key= True, index=True)
+    name = Column(String(255), nullable=False)
+    address = Column(String(400))
+    min_price = Column(Integer, default=100)
+    max_queue_size = Column(Integer, default=15)
+    qr_code_id = Column(String(100), unique=True, nullable=False, index=True)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime, server_default=func.now())
+
+    orders = relationship("Order", back_populates="venue", cascade="all, delete-orphan")
+    play_logs = relationship("PlayLog", back_populates="venue", cascade="all, delete-orphan")
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer, primary_key=True, index=True)
+    venue_id = Column(Integer, ForeignKey("venues.id"), nullable=False, index=True)
+
+    song_title = Column(String(500), nullable=False)
+    song_artist = Column(String(70))
+    #song_external_id возможно, если у музыки из внешних сервисов будет id
+    song_provider = Column(String(50))
+    song_duration = Column(Integer)
+
+    price_paid = Column(Integer, nullable=False)
+    payment_id = Column(String(255))
+
+    status = Column(Enum(OrderStatus), default=OrderStatus.PENDING, index=True)
+
+    venue = relationship("Venue", back_populates="orders")
+
+class PlayLog(Base):
+    __tablename__ = 'play_log'
+    id = Column(Integer, nullable=False, index=True, primary_key=True)
+    venue_id = Column(Integer, ForeignKey("venues.id"), nullable=False, index=True)
+    order_id = Column(Integer, ForeignKey('orders.id'), nullable=False, index=True)
+
+    song_title = Column(String(500))
+    song_artist = Column(String(500))
+    song_provider = Column(String(50))
+    #song_external_id = Column(String(255))
+
+    skipped = Column(Boolean, default=False)
+    price_paid = Column(Integer)
+
+    venue = relationship("Venue", back_populates="play_logs")
---
+++ app/database/schemas.py
@@ -0,0 +1,94 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from datetime import datetime
+from enum import Enum
+
+class OrderStatus(str, Enum):
+    PENDING = "pending"
+    PAID = "paid"
+    PLAYED = "played"
+    SKIPPED = "skipped"
+    CANCELLED = "cancelled"
+
+
+#пускай пока просто висит, в дальнейшем может пригодиться
+class SongProvider(str, Enum):
+    YOUTUBE = "youtube"
+    VK = "vk"
+    YANDEX = "yandex"
+
+
+class VenueBase(BaseModel):
+    name: str
+    address: Optional[str] = None
+    min_prise: int = Field(default=100, ge=0)
+    max_queue_size: int = Field(default=15, gt=1)
+    qr_code_id: str
+    is_active: bool = True
+
+class VenueCreate(VenueBase):
+    pass
+
+class VenueUpdate(BaseModel):
+    name: Optional[str] = None
+    address: Optional[str] = None
+    min_prise: Optional[int] = Field(default=None, ge=0)
+    max_queue_size: Optional[int] = Field(default=None, gt=1    )
+    is_active: Optional[bool] = None
+
+class VenueResponse(VenueBase):
+    id: int
+    created_at: datetime
+    updated_at: Optional[datetime] = None
+    
+    class Config:
+        from_attributes = True
+
+
+class OrderBase(BaseModel):
+    song_title: str = Field(max_length=500)
+    song_artist: Optional[str] = Field(default=None, max_length=70)
+    song_provider: SongProvider
+    song_duration: Optional[int] = Field(default=None, ge=0)
+
+
+class OrderCreate(OrderBase):
+    pass
+
+class OrderResponse(BaseModel):
+    id: int
+    venue_id: int
+    song_title: str
+    song_artist: Optional[str] = None
+    song_provider: Optional[str] = None
+    song_duration: Optional[int] = None
+    price_paid: int
+    payment_id: Optional[str] = None
+    status: OrderStatus
+    created_at: datetime
+    
+    class Config:
+        from_attributes = True
+
+
+class OrderStatusUpdate(BaseModel):
+    status: OrderStatus
+    skip_reason: Optional[str] = Field(default=None, max_length=500)
+
+class PlayLogResponse(BaseModel):
+    id: int
+    venue_id: int
+    order_id: int
+    song_title: Optional[str] = None
+    song_artist: Optional[str] = None
+    song_provider: Optional[str] = None
+    skipped: bool
+    price_paid: Optional[int] = None
+    
+    class Config:
+        from_attributes = True
+
+#добавить схемы очереди
+#добавить схемы платежной системы
+#добавить схему текущего трека
+#добавить схему поиска
